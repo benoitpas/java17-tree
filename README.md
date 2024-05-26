3ab966468248e5d397bf4d93d80324b10ab75b51
benoitpas <bpasquereau@gmail.com>
2024-05-26 09:20:50 +0100

    use java17 functionality

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 Project to test implementation of a function that adds an id to a tree using the latest java features.
 
-The main change is the introduction of the 'record' which is used to model the 'leaf' and the 'branch' classes.
+The main change compared to the [java 15 project](https://github.com/benoitpas/java15-tree) is:
+* using sealed classes (it was already available in Java15 preview)
+* using the 'switch' (from java 17 preview)[https://docs.oracle.com/en/java/javase/17/language/pattern-matching-switch-expressions-and-statements.html]
+
+The code is starting to look a bit more functional but the limited type inference makes the function signatures look not very elegant.
--- src/main/java/org/benoit/Tree.java
+++ src/main/java/org/benoit/Tree.java
@@ -3,37 +3,45 @@ package org.benoit;
 import java.util.AbstractMap;
 
 public class Tree<T> {
-    // The Tree class should be made a singleton but doesn't seem to work with the type parameters
-    public interface INode<T> {
+    // The Tree class should be made a singleton but doesn't with the type parameters
+    // Using a sealed class (introduced in Java17)
+    public sealed interface INode<T> permits Branch, Leaf {
     }
     public record Branch<T>(T value, INode<T> left, INode<T> right) implements INode<T> {
     }
-
     public record Leaf<T>() implements INode<T> {
     }
-
-    // Move declaration of Leaf/Branch here
 
     public INode<AbstractMap.SimpleEntry<Integer,T>> addId(INode<T> node) {
         return addId(node,0).getValue();
     }
-    
+
+    // Because it is not possible to declare a variable in an expression in Java, I had to write this provate class
+    // In Scala the code could have put in the case statement
+    private AbstractMap.SimpleEntry<Integer,INode<AbstractMap.SimpleEntry<Integer,T>>> newBranch(Branch<T> b, Integer start)  {
+        var newLeft = addId(b.left(), start);
+        var newRight = addId(b.right(), newLeft.getKey());
+        var newBranch = new Branch<>(
+            new AbstractMap.SimpleEntry<>(newRight.getKey(), b.value()),
+                newLeft.getValue(),
+                newRight.getValue());
+        return new AbstractMap.SimpleEntry<>(newRight.getKey() + 1, newBranch);
+    }
+
+    // Here we also need to create a function because of the variable leaf. Also because we need to specify the type of
+    // the function, the syntax is not very elegant.
+    private AbstractMap.SimpleEntry<Integer, INode<AbstractMap.SimpleEntry<Integer,T>>> newLeaf(Integer start) {
+        final INode<AbstractMap.SimpleEntry<Integer,T>> leaf = new Leaf<>();
+        return new AbstractMap.SimpleEntry<>(start, leaf);
+    }
+
+    // Replace AbstractMap.SimpleEntry with record
     AbstractMap.SimpleEntry<Integer,INode<AbstractMap.SimpleEntry<Integer,T>>>
         addId(INode<T> node, Integer start) {
-        INode<AbstractMap.SimpleEntry<Integer,T>> l = new Leaf<>();
-        var r = new AbstractMap.SimpleEntry<>(start, l);
-        // Waiting for switch on types (https://openjdk.java.net/jeps/8213076)
-        // but still nice to use JEP 394 (pattern matching for instanceof)
-        if (node instanceof Branch<T> b) {
-            var newLeft = addId(b.left(), start);
-            var newRight = addId(b.right(), newLeft.getKey());
-            var newBranch = new Branch<>(
-                    new AbstractMap.SimpleEntry<>(newRight.getKey(), b.value()),
-                    newLeft.getValue(),
-                    newRight.getValue()
-            );
-            r = new AbstractMap.SimpleEntry<>(newRight.getKey() + 1, newBranch);
-        }
-        return r;
+        // The switch starts to look good !
+        return switch(node) {
+            case Branch<T> b -> newBranch(b, start);
+            default -> newLeaf(start);
+        };
     }
 }
