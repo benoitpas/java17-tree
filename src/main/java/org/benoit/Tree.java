67cb156bfea7b83c776403fd1cc5a4ca94a90e9f
benoitpas <bpasquereau@gmail.com>
2024-05-29 19:37:20 +0100

    Replacement of AbstractMap.SimpleEntry

--- src/main/java/org/benoit/Tree.java
+++ src/main/java/org/benoit/Tree.java
@@ -1,9 +1,7 @@
 package org.benoit;
 
-import java.util.AbstractMap;
-
 public class Tree<T> {
-    // The Tree class should be made a singleton but doesn't with the type parameters
+    // The Tree class should be made a singleton but it is not possible because of the type parameters
     // Using a sealed class (introduced in Java17)
     public sealed interface INode<T> permits Branch, Leaf {
     }
@@ -12,31 +10,34 @@ public class Tree<T> {
     public record Leaf<T>() implements INode<T> {
     }
 
-    public INode<AbstractMap.SimpleEntry<Integer,T>> addId(INode<T> node) {
-        return addId(node,0).getValue();
+    public record WithId<T>(Integer id, T value) {
+    }
+
+    public INode<WithId<T>> addId(INode<T> node) {
+        return addId(node,0).value();
     }
 
     // Because it is not possible to declare a variable in an expression in Java, I had to write this provate class
     // In Scala the code could have put in the case statement
-    private AbstractMap.SimpleEntry<Integer,INode<AbstractMap.SimpleEntry<Integer,T>>> newBranch(Branch<T> b, Integer start)  {
+    private WithId<INode<WithId<T>>> newBranch(Branch<T> b, Integer start)  {
         var newLeft = addId(b.left(), start);
-        var newRight = addId(b.right(), newLeft.getKey());
+        var newRight = addId(b.right(), newLeft.id());
         var newBranch = new Branch<>(
-            new AbstractMap.SimpleEntry<>(newRight.getKey(), b.value()),
-                newLeft.getValue(),
-                newRight.getValue());
-        return new AbstractMap.SimpleEntry<>(newRight.getKey() + 1, newBranch);
+            new WithId(newRight.id(), b.value()),
+                newLeft.value(),
+                newRight.value());
+        return new WithId(newRight.id() + 1, newBranch);
     }
 
     // Here we also need to create a function because of the variable leaf. Also because we need to specify the type of
     // the function, the syntax is not very elegant.
-    private AbstractMap.SimpleEntry<Integer, INode<AbstractMap.SimpleEntry<Integer,T>>> newLeaf(Integer start) {
-        final INode<AbstractMap.SimpleEntry<Integer,T>> leaf = new Leaf<>();
-        return new AbstractMap.SimpleEntry<>(start, leaf);
+    private WithId<INode<WithId<T>>> newLeaf(Integer start) {
+        final INode<WithId<T>> leaf = new Leaf<>();
+        return new WithId(start, leaf);
     }
 
     // Replace AbstractMap.SimpleEntry with record
-    AbstractMap.SimpleEntry<Integer,INode<AbstractMap.SimpleEntry<Integer,T>>>
+    WithId<INode<WithId<T>>>
         addId(INode<T> node, Integer start) {
         // The switch starts to look good !
         return switch(node) {
--- src/test/java/org/benoit/TreeTest.java
+++ src/test/java/org/benoit/TreeTest.java
@@ -3,25 +3,18 @@ package org.benoit;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import java.util.AbstractMap;
+import org.benoit.Tree.WithId;
 
 
 public class TreeTest {
 
-    // Could be added to 'Tree' to simplify the code and made public to
-    // have it accessible from unit tests. As it doesn't have side effects,
-    // it will not create issues to have it public.
-    AbstractMap.SimpleEntry<Integer, String> pair(Integer i, String v) {
-        return new AbstractMap.SimpleEntry<>(i, v);
-    }
-
-    @Test
     public void testAddId() {
         Tree.Leaf l = new Tree.Leaf();
-        Tree.Branch expectedtb = new Tree.Branch(pair(4, "a"),
-                new Tree.Branch(pair(0, "b"), l, l),
-                new Tree.Branch(pair(3, "c"),
-                        new Tree.Branch(pair(1, "d"), l, l),
-                        new Tree.Branch(pair(2, "e"), l, l)));
+        Tree.Branch expectedtb = new Tree.Branch(new Tree.WithId(4, "a"),
+                new Tree.Branch(new Tree.WithId(0, "b"), l, l),
+                new Tree.Branch(new Tree.WithId(3, "c"),
+                        new Tree.Branch(new Tree.WithId(1, "d"), l, l),
+                        new Tree.Branch(new Tree.WithId(2, "e"), l, l)));
 
         Tree t = new Tree();
         assertEquals(expectedtb, t.addId(App.tb));
